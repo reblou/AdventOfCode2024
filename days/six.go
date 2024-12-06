5fdd1a169e82f3937bbe1fa5bcd3ebdd04fbce93
Rebecca <rebeayto@gmail.com>
2024-12-06 21:28:04 +0000

    part two solution

--- days/six.go
+++ days/six.go
@@ -15,10 +15,18 @@ type coord struct {
 	y int
 }
 
+type moveInfo struct {
+	location coord
+	vector   coord
+}
+
 func Six() {
 	fmt.Println("-Day Six-")
 
-	fmt.Printf("Part 1: %v\n", countGuardPath(input6))
+	guardPath := makeGuardPath(input6)
+	fmt.Printf("Part 1: %v\n", countXs(guardPath))
+
+	fmt.Printf("Part 2: %v\n", countLoopObsticles(parseInput6(input6), guardPath))
 }
 
 func parseInput6(input string) [][]string {
@@ -47,13 +55,9 @@ func printOutput(input [][]string) {
 	}
 }
 
-func countGuardPath(input string) int {
+func makeGuardPath(input string) [][]string {
 	parsed := parseInput6(input6)
-	printOutput(parsed)
-
 	guardPos := getGuardPosition(parsed)
-
-	fmt.Printf("Guard Position: %v\n", guardPos)
 
 	guardMoveVector := coord{0, -1}
 
@@ -64,7 +68,7 @@ func countGuardPath(input string) int {
 		layout, err = moveGuard(layout, &guardPos, &guardMoveVector)
 	}
 
-	return countXs(layout)
+	return layout
 }
 
 func getGuardPosition(input [][]string) coord {
@@ -120,4 +124,100 @@ func countXs(input [][]string) int {
 		}
 	}
 	return count
+}
+
+func countLoopObsticles(input [][]string, guardPath [][]string) int {
+	guardStart := getGuardPosition(input)
+	//foreach X, check if making it an # creates a loop
+	xs := getXs(guardPath)
+
+	var count int
+	for _, x := range xs {
+		if makesLoop(guardPath, x, guardStart) {
+			count++
+		}
+	}
+
+	return count
+}
+
+func getXs(layout [][]string) []coord {
+	var coords []coord
+	for y, line := range layout {
+		for x, char := range line {
+			if char == "X" {
+				coords = append(coords, coord{x, y})
+			}
+		}
+	}
+	return coords
+}
+
+func makesLoop(layout [][]string, obsticle coord, guardStart coord) bool {
+	//run steps, check if loop, record seen #s
+	guardPos := guardStart
+	guardMoveVector := coord{0, -1}
+
+	pivots := make(map[int]map[int][]moveInfo)
+	for i := range len(layout) {
+		pivots[i] = make(map[int][]moveInfo)
+	}
+	newlayout := sliceSliceDeepCopy(layout)
+
+	newlayout[obsticle.y][obsticle.x] = "#"
+
+	guardPos, err := nextGuardPos(newlayout, &guardPos, &guardMoveVector, pivots)
+
+	for err == nil {
+		//printOutput(newlayout)
+		guardPos, err = nextGuardPos(newlayout, &guardPos, &guardMoveVector, pivots)
+	}
+
+	return errors.Is(err, ErrLoop)
+}
+
+var ErrOOB = fmt.Errorf("out of bounds")
+var ErrLoop = fmt.Errorf("visited before")
+
+func nextGuardPos(input [][]string, guard *coord, vec *coord, pivots map[int]map[int][]moveInfo) (coord, error) {
+	width := len(input[0])
+
+	next := coord{guard.x + vec.x, guard.y + vec.y}
+
+	if next.x < 0 || next.x >= width || next.y < 0 || next.y >= len(input) {
+		return next, ErrOOB
+	}
+
+	if input[next.y][next.x] == "#" {
+		*vec = rotateGuard(*vec)
+		prevs := pivots[guard.x][guard.y]
+		if sliceContainsMove(prevs, *guard, *vec) {
+			return next, ErrLoop
+		}
+
+		pivots[guard.x][guard.y] = append(pivots[guard.x][guard.y], moveInfo{*guard, *vec})
+		return nextGuardPos(input, guard, vec, pivots)
+	}
+
+	return next, nil
+}
+
+func sliceContainsMove(s []moveInfo, n coord, v coord) bool {
+	for _, m := range s {
+		if m.location.x == n.x && m.location.y == n.y && m.vector.x == v.x && m.vector.y == v.y {
+			return true
+		}
+	}
+
+	return false
+}
+
+func sliceSliceDeepCopy(s [][]string) [][]string {
+	var new [][]string
+
+	for _, l := range s {
+		new = append(new, append([]string{}, l...))
+	}
+
+	return new
 }
